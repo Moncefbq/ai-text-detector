5b680c4ab08ade82369e8e54b0083e7765378fbb
Moncefbq <127325944+Moncefbq@users.noreply.github.com>
2026-05-31 22:12:03 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from backend.api.detect import analyze_text
 
 print("MAIN FILE LOADED")
@@ -8,6 +9,17 @@ app = FastAPI(
     title="AICheck Pro API",
     description="Advanced AI-generated text detection API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class TextRequest(BaseModel):
